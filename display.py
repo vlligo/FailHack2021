6050756a296d1829fde2c9786e0e7a04bdc790ee
vlligo <vladimir2balakin@yandex.ru>
2021-06-09 21:12:17 +0300

    Added two points on one map

--- display.py
+++ display.py
@@ -2,40 +2,35 @@ import os
 import webbrowser
 
 import folium
-import wikipedia as wk
-import requests
-from bs4 import BeautifulSoup
-import re
-import numpy as np
-
-wk.set_lang("en")
+import geopy
 
 
 def get_coordinates(place):
-    url = wk.page(place).url
-    print(url)
-    response = requests.get(url)
-    if response:
-        try:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            lat_lon = soup.find("span", class_="geo-dec").text
-            lat_lon_dd = [float(i) for i in re.findall("\\d+[.]\\d+", lat_lon)]
-            if re.findall("\\w$", lat_lon)[0] == "S":
-                lat_lon_dd[0] = -1 * lat_lon_dd[0]
-            if re.findall("\\w$", lat_lon)[0] == "W":
-                lat_lon_dd[1] = -1 * lat_lon_dd[1]
-            lat, lon = lat_lon_dd
-        except AttributeError:
-            print("WARNING: No such location {}.".format(place))
-            lat, lon = np.nan, np.nan
-    else:
-        print("WARNING: No such location {}.".format(place))
-        lat, lon = np.nan, np.nan
-    return [lat, lon]
+    locator = geopy.Nominatim(user_agent="myGeocoder")
+    location = locator.geocode(place)
+    return [location.latitude, location.longitude]
 
 
 def show(title):
-    m = folium.Map(location=get_coordinates(title))
+    coordinates = get_coordinates(title)
+    m = folium.Map()
+    tooltip = title
+    folium.Marker(
+        get_coordinates(title), popup="<b>Timberline Lodge</b>", tooltip=tooltip
+    ).add_to(m)
+    filename = "index.html"
+    m.save(filename)
+    webbrowser.open('file://' + os.path.realpath(filename))
+
+
+def show_array(titles):
+    m = folium.Map()
+    for title in titles:
+        coordinates = get_coordinates(title)
+        tooltip = title
+        folium.Marker(
+            get_coordinates(title), popup="<b>Timberline Lodge</b>", tooltip=tooltip
+        ).add_to(m)
     filename = "index.html"
     m.save(filename)
     webbrowser.open('file://' + os.path.realpath(filename))
--- main.py
+++ main.py
@@ -1,1 +1,60 @@
-from display import show
+import json
+
+from display import show, show_array
+import tkinter as tk
+
+name, surname = "", ""
+i = 0
+while name == "" or surname == "":
+    root = tk.Tk()
+    root.title("Log in")
+    root.geometry("640x480")
+    both = tk.IntVar()
+    both.set(0)
+
+    tk.Label(root, text="Name:").grid(row=0)
+    name_entry = tk.Entry(root)
+    name_entry.grid(row=0, column=1)
+    tk.Label(root, text="*", fg="red", font="Arial 10").grid(row=0, column=2)
+    tk.Label(root, text="Surname:").grid(row=1)
+    surname_entry = tk.Entry(root)
+    surname_entry.grid(row=1, column=1)
+    tk.Label(root, text="*", fg="red", font="Arial 10").grid(row=1, column=2)
+    tk.Radiobutton(root, text="Show both on one map", variable=both, value=0).place(relx=0.0, rely=0.1)
+    tk.Radiobutton(root, text="Show two separate maps", variable=both, value=1).place(relx=0.3, rely=0.1)
+
+
+    def submitted():
+        global name, surname
+        name = name_entry.get()
+        surname = surname_entry.get()
+        root.destroy()
+
+
+    submit_button = tk.Button(root, text="Submit", command=submitted).place(relx=0.0, rely=0.15)
+    if i > 0:
+        tk.Label(root, text="* You must enter name and surname", font="Arial 8", fg="red").place(relx=0.0, rely=0.225)
+    root.mainloop()
+    i += 1
+
+with open("objects.json", "r") as input_file:
+    places = json.load(input_file)
+
+
+def get_name(word):
+    left = -1
+    right = len(places) - 1
+    while right - left > 1:
+        m = (right + left) // 2
+        if places[m] >= word:
+            right = m
+        else:
+            left = m
+    return places[right]
+
+
+if both.get() == 1:
+    show(get_name(name))
+    show(get_name(surname))
+else:
+    show_array([get_name(name), get_name(surname)])
